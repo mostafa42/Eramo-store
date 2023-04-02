ce8739144edb27603dc6748a3cd1b04f20f6b179
AmrTarekAbuElkhair <engamrtarek1992@gmail.com>
2023-04-02 02:52:13 +0200

    update in product reviews migration

--- database/migrations/2022_12_22_085502_create_product_reviews_table.php
+++ database/migrations/2022_12_22_085502_create_product_reviews_table.php
@@ -16,9 +16,14 @@ return new class extends Migration
         Schema::create('product_reviews', function (Blueprint $table) {
             $table->id();
             $table->string('title')->nullable();
-            $table->text('review')->nullable();
-            $table->enum('rating', [1, 2, 3, 4, 5]);
-            $table->enum('approved',[1,0])->default(0)->nullable();
+            // $table->text('review')->nullable();
+            // $table->enum('rating', [1, 2, 3, 4, 5]);
+            // $table->enum('approved',[1,0])->default(0)->nullable();
+            $table->string('name');
+            $table->string('email');
+            $table->string('title');
+            $table->string('testimonial');
+            $table->tinyInteger('status')->nullable();
             $table->unsignedBigInteger('user_id')->nullable();
             $table->foreign('user_id')->references('id')->on('users')->cascadeOnUpdate()->cascadeOnDelete();
             $table->unsignedBigInteger('product_id')->nullable();
